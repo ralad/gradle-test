a8dee5999dfe6101d61672d41da024f32583f365
Andreas <Andreas@DESKTOP-U6JJE9P>
2015-11-24 23:36:51 +0100

    Added person3

---
+++ gradle-test-project/src/main/java/org/gradle/Person3.java
@@ -0,0 +1,30 @@
+package org.gradle;
+
+import org.apache.commons.collections.list.GrowthList;
+
+public class Person3 {
+    
+    private final String lastName;
+    
+    private final String fistName;
+
+    public Person3(String lastName, final String fistName) {
+        this.lastName = lastName;
+        this.fistName = fistName;
+        new GrowthList();
+    }
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @return the fistName
+	 */
+	public String getFistName() {
+		return fistName;
+	}
+}
