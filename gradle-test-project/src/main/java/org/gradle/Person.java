5c1c7131bd66ff15c1b448169ec6e91bbe06f18b
Andreas <Andreas@DESKTOP-U6JJE9P>
2015-11-24 23:03:07 +0100

    Added lastName to Person

--- gradle-test-project/src/main/java/org/gradle/Person.java
+++ gradle-test-project/src/main/java/org/gradle/Person.java
@@ -5,13 +5,23 @@ import org.joda.time.DateTime;
 
 public class Person {
     private final String name;
+    
+    private final String lastName;
 
-    public Person(String name) {
+    public Person(String name, String lastName) {
         this.name = name;
+        this.lastName = lastName;
         new GrowthList();
     }
 
     public String getName() {
         return name;
     }
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
 }
