aa8e002b53ec07cab67f923f4e516dbf4cb112a4
Andreas <Andreas@something.com>
2015-11-24 23:12:59 +0100

    Removed name and jodatime from Person

--- gradle-test-project/src/main/java/org/gradle/Person.java
+++ gradle-test-project/src/main/java/org/gradle/Person.java
@@ -1,24 +1,17 @@
 package org.gradle;
 
 import org.apache.commons.collections.list.GrowthList;
-import org.joda.time.DateTime;
 
 public class Person {
-    private final String name;
     
     private final String lastName;
     
     private final String fistName;
 
-    public Person(String name, String lastName, final String fistName) {
-        this.name = name;
+    public Person(String lastName, final String fistName) {
         this.lastName = lastName;
         this.fistName = fistName;
         new GrowthList();
-    }
-
-    public String getName() {
-        return name;
     }
 
 	/**
